c31457ad8bae733b4e1ae337c716a6ed3d6ad159
Yauheni Kuntsevich <cmaczok@gmail.com>
2026-02-12 16:25:01 +0300

    feat: add logger

--- cmd/shortest-url/main.go
+++ cmd/shortest-url/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 
 	"github.com/cmczk/shortest-url/internal/config"
+	"github.com/cmczk/shortest-url/internal/logger"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := logger.Setup(cfg.Env)
+
+	log.Info("starting shortest-url", slog.String("env", cfg.Env))
+	log.Debug("debug messages enabled")
 
 	// TODO: init logger
 	// TODO: init storage
